ef3ab8d70aed4a020f3049e86aa427d36a7e9f14
William Gao <wgao1@umbc.edu>
2019-10-29 16:16:16 -0400

    Setup collection view

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by William Gao on 10/28/19.
+//  Copyright © 2019 willgao. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
